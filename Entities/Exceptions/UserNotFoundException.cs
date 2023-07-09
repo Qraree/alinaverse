c1caf73e86769d4873828192b3b8348b0cf1ed97
Hiccstrid2019 <hiccup.colin@yandex.ru>
2023-07-09 23:19:54 +0300

    added exceptions for auth

---
+++ Entities/Exceptions/UserNotFoundException.cs
@@ -0,0 +1,8 @@
+﻿namespace Entities.Exceptions;
+
+public class UserNotFoundException : NotFoundException
+{
+    public UserNotFoundException(string email) 
+        : base($"User with email {email} not found")
+    {}
+}
