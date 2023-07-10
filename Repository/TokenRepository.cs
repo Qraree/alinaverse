f789580eb1b1f2d090b6cd597eaf82ada638e0db
Hiccstrid2019 <hiccup.colin@yandex.ru>
2023-07-10 20:05:14 +0300

    added a repository for working with tokens

---
+++ Repository/TokenRepository.cs
@@ -0,0 +1,44 @@
+﻿using System.Data;
+using Contracts;
+using Dapper;
+using Entities.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace Repository;
+
+public class TokenRepository : RepositoryBase, ITokenRepository
+{
+    public TokenRepository(IConfiguration configuration)
+        : base(configuration)
+    { }
+
+    public Token GetRefreshToken(int userId, string token)
+    {
+        using (IDbConnection dbConnection = Connection)
+        {
+            dbConnection.Open();
+            return dbConnection.Query<Token>("SELECT * FROM refresh_tokens WHERE user_id = @Id " +
+                                             "AND refresh_token = @Token", new {Id = userId, Token = token})
+                .FirstOrDefault();
+        }
+    }
+
+    public void SetUpRefreshToken(Token token)
+    {
+        using (IDbConnection dbConnection = Connection)
+        {
+            dbConnection.Open();
+            dbConnection.Execute("INSERT INTO refresh_tokens (user_id, refresh_token, expiry_time) " +
+                                 "VALUES (@UserId, @RefreshToken, @ExpiryTime)", token);
+        }
+    }
+
+    public void DeleteRefreshToken(int id)
+    {
+        using (IDbConnection dbConnection = Connection)
+        {
+            dbConnection.Open();
+            dbConnection.Execute("DELETE FROM refresh_tokens WHERE id = @Id", new {Id = id});
+        }
+    }
+}
