bc9bb4f356b42d51a90109ee105da9e73c060dbe
Hiccstrid2019 <hiccup.colin@yandex.ru>
2023-07-11 00:15:11 +0300

    modify docker-compose, updated init.sql script and reference for project

--- init.sql
+++ init.sql
@@ -5,7 +5,7 @@
 -- Dumped from database version 15.2
 -- Dumped by pg_dump version 15.2
 
--- Started on 2023-07-08 19:00:48
+-- Started on 2023-07-11 00:03:23
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -19,11 +19,11 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
--- TOC entry 3323 (class 1262 OID 669824)
+-- TOC entry 3332 (class 1262 OID 669824)
 -- Name: alinaverse; Type: DATABASE; Schema: -; Owner: postgres
 --
 
-CREATE DATABASE alinaverse WITH TEMPLATE = template0 ENCODING = 'UTF8';
+CREATE DATABASE alinaverse ENCODING = 'UTF8';
 
 
 ALTER DATABASE alinaverse OWNER TO postgres;
@@ -46,6 +46,46 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
+-- TOC entry 217 (class 1259 OID 669839)
+-- Name: refresh_tokens; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.refresh_tokens (
+    id integer NOT NULL,
+    user_id integer NOT NULL,
+    refresh_token character varying NOT NULL,
+    expiry_time timestamp without time zone NOT NULL
+);
+
+
+ALTER TABLE public.refresh_tokens OWNER TO postgres;
+
+--
+-- TOC entry 216 (class 1259 OID 669838)
+-- Name: refreshTokens_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public."refreshTokens_id_seq"
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public."refreshTokens_id_seq" OWNER TO postgres;
+
+--
+-- TOC entry 3333 (class 0 OID 0)
+-- Dependencies: 216
+-- Name: refreshTokens_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public."refreshTokens_id_seq" OWNED BY public.refresh_tokens.id;
+
+
+--
 -- TOC entry 215 (class 1259 OID 669826)
 -- Name: users; Type: TABLE; Schema: public; Owner: postgres
 --
@@ -53,7 +93,8 @@ SET default_table_access_method = heap;
 CREATE TABLE public.users (
     id integer NOT NULL,
     name character varying NOT NULL,
-    password character varying NOT NULL
+    password character varying NOT NULL,
+    email character varying NOT NULL
 );
 
 
@@ -76,7 +117,7 @@ CREATE SEQUENCE public.user_id_seq
 ALTER TABLE public.user_id_seq OWNER TO postgres;
 
 --
--- TOC entry 3324 (class 0 OID 0)
+-- TOC entry 3334 (class 0 OID 0)
 -- Dependencies: 214
 -- Name: user_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
@@ -85,7 +126,15 @@ ALTER SEQUENCE public.user_id_seq OWNED BY public.users.id;
 
 
 --
--- TOC entry 3173 (class 2604 OID 669829)
+-- TOC entry 3179 (class 2604 OID 669842)
+-- Name: refresh_tokens id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.refresh_tokens ALTER COLUMN id SET DEFAULT nextval('public."refreshTokens_id_seq"'::regclass);
+
+
+--
+-- TOC entry 3178 (class 2604 OID 669829)
 -- Name: users id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -93,7 +142,16 @@ ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.user_i
 
 
 --
--- TOC entry 3175 (class 2606 OID 669833)
+-- TOC entry 3183 (class 2606 OID 669846)
+-- Name: refresh_tokens refreshTokens_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.refresh_tokens
+    ADD CONSTRAINT "refreshTokens_pkey" PRIMARY KEY (id);
+
+
+--
+-- TOC entry 3181 (class 2606 OID 669833)
 -- Name: users user_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -101,7 +159,16 @@ ALTER TABLE ONLY public.users
     ADD CONSTRAINT user_pkey PRIMARY KEY (id);
 
 
--- Completed on 2023-07-08 19:00:48
+--
+-- TOC entry 3184 (class 2606 OID 669847)
+-- Name: refresh_tokens user_id_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.refresh_tokens
+    ADD CONSTRAINT user_id_fk FOREIGN KEY (user_id) REFERENCES public.users(id);
+
+
+-- Completed on 2023-07-11 00:03:26
 
 --
 -- PostgreSQL database dump complete
