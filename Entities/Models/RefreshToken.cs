66a16487dda7d0e8be16ed3457370ace99e684a9
Hiccstrid2019 <hiccup.colin@yandex.ru>
2023-07-10 20:12:39 +0300

    map column name to properties

---
+++ Entities/Models/RefreshToken.cs
@@ -0,0 +1,16 @@
+﻿
+
+using Shared.DapperCustomAttributes;
+
+namespace Entities.Models;
+
+public class Token
+{
+    public int Id { get; set; }
+    [Column(Name = "user_id")]
+    public int UserId { get; set; }
+    [Column(Name = "refresh_token")]
+    public string RefreshToken { get; set; }
+    [Column(Name = "expiry_time")]
+    public DateTime ExpiryTime { get; set; }
+}
--- Entities/Models/User.cs
+++ Entities/Models/User.cs
@@ -4,10 +4,8 @@ namespace Entities.Models;
 
 public class User
 {
-    [Key]
     public int Id { get; set; }
-    [Required]
     public string Name { get; set; }
-    [Required]
+    public string Email { get; set; }
     public string Password { get; set; }
 }
